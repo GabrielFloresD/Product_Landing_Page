cefe85e04175de88d1f9a4e230998b90960e250f
Gabriel Flores <gabrielf.developer@gmail.com>
2023-08-28 16:33:06 -0300

    Added styles for container_info and the event for the vertical tab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const tabs = document.querySelectorAll(".info__li");
+
+tabs.forEach((tab,index) => {
+    tab.addEventListener("click", () => {
+        let sectionDrink = tab.parentElement.parentElement.parentElement.parentElement;
+        if (sectionDrink.id === "drink1") {
+            let tabContents = document.querySelectorAll("#drink1 .tab-content");
+            tabContents.forEach(content => {
+                content.classList.remove("active");
+            });
+            tabs.forEach((tab) => {
+                tab.classList.remove("active");
+            });
+            tabContents[index].classList.add("active");
+            tabs[index].classList.add("active");
+        };
+    });
+});
+
